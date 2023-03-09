d1cba2a0332b364d202e2cf5d6b0b7bcdd3ac244
Mauricio Urrego <mauricio@dotdotdot.email>
2023-03-09 16:35:50 +0100

    Added static data.

--- frontend/src/app/products/application/fetchProductService.ts
+++ frontend/src/app/products/application/fetchProductService.ts
@@ -1,7 +1,16 @@
 import { product } from '../domain/product';
+import { coffees, teas } from './static';
 const API_BASE_URL = process.env.API_BASE_URL || 'https://example.com/api';
 
 export async function fetchProductService(type?: string | string[]): Promise<product[]> {
+  if (API_BASE_URL === '/static') {
+    if (type === 'coffees') {
+      return coffees;
+    }
+    else if (type === 'teas') {
+      return teas;
+    }
+  }
   try {
     const response = await fetch(`${API_BASE_URL}${type}`);
     if (!response.ok) {
---
+++ frontend/src/app/products/application/static.ts
@@ -0,0 +1,101 @@
+export const coffees = [
+    {
+        id: 9,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/1.png',
+        type: 'Arabic'
+    },
+    {
+        id: 10,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/2.png',
+        type: 'Robusta'
+    },
+    {
+        id: 11,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/3.png',
+        type: 'Arabic'
+    },
+    {
+        id: 12,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/4.png',
+        type: 'Robusta'
+    },
+    {
+        id: 13,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/5.png',
+        type: 'Robusta'
+    },
+    {
+        id: 14,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/6.png',
+        type: 'Arabic'
+    },
+    {
+        id: 15,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/7.png',
+        type: 'Robusta'
+    },
+    {
+        id: 16,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/8.png',
+        type: 'Arabic'
+    }
+];
+
+export const teas = [
+    {
+        id: 9,
+        title: 'GEPA Chiapas',
+        imageUrl: '/images/tea1.png',
+        type: ''
+    },
+    {
+        id: 10,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/tea2.png',
+        type: ''
+    },
+    {
+        id: 11,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/tea3.png',
+        type: ''
+    },
+    {
+        id: 12,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/tea4.png',
+        type: ''
+    },
+    {
+        id: 13,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/tea5.png',
+        type: ''
+    },
+    {
+        id: 14,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/tea6.png',
+        type: ''
+    },
+    {
+        id: 15,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/tea7.png',
+        type: ''
+    },
+    {
+        id: 16,
+        title: 'GEPA Chiapas with two lines',
+        imageUrl: '/images/tea8.png',
+        type: ''
+    }
+];
