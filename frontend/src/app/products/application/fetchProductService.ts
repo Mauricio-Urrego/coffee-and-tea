d5d67cf5c0e98decd57b236d70bd891c74a01082
Mauricio Urrego <mauricio@dotdotdot.email>
2023-03-09 13:26:57 +0100

    Updated .env variable usage.

--- frontend/src/app/products/application/fetchProductService.ts
+++ frontend/src/app/products/application/fetchProductService.ts
@@ -1,8 +1,9 @@
 import { product } from '../domain/product';
+const API_BASE_URL = process.env.API_BASE_URL || 'https://example.com/api';
 
 export async function fetchProductService(type?: string | string[]): Promise<product[]> {
   try {
-    const response = await fetch(`${process.env.API_BASE_URL}${type}`);
+    const response = await fetch(`${API_BASE_URL}${type}`);
     if (!response.ok) {
       throw new Error('Failed to fetch products.');
     }
